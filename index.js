9376b587f3c87f7e15eacdc8fdec4bdc285ce8f0
amembrides <antonio.membrides@sita.aero>
2023-08-20 14:49:41 +0200

    add menu

--- index.js
+++ index.js
@@ -11,6 +11,19 @@ app.use('/github', require('./route/route.github'));
 app.use('/telegram', require('./route/route.telegram'));
 app.use('/whatsapp', require('./route/route.whatsapp'));
 
+// home 
+app.use('/', (req, res) => {
+  res.send(`<ul>
+    <li><a href="/google/login">GOOGLE</a></li>
+    <li><a href="/github/login">GITHUB</a></li>
+    <li><a href="/facebook/login">FACEBOOK</a></li>
+    <li> ---- </li>
+    <li><a href="/telegram/info">TELEGRAM</a></li>
+    <li><a href="/whatsapp/info">WHATSAPP</a></li>
+  </ul>`);
+});
+
+
 // Inicia el servidor
 app.listen(PORT, () => {
   console.log(`Servidor en ejecución en http://localhost:${PORT}`);
